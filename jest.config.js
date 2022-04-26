bd2df50fa9b4cf4fb9cc9142ff0b3c58b8edf04e
miruoon <rillakerika@gmail.com>
2022-04-26 23:32:53 +0900

    jestの設定をした

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
